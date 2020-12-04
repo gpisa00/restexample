cdf938c0548031a84f4de4da90f203d44ff0d727
gpisa00 <giuseppepisa00@gmail.com>
2020-12-04 19:19:02 +0100

    change version java

--- src/main/java/it/arteprogrammazione/restexample/commons/dto/RequestCustomerDTO.java
+++ src/main/java/it/arteprogrammazione/restexample/commons/dto/RequestCustomerDTO.java
@@ -9,12 +9,12 @@ public class RequestCustomerDTO {
 
     @NotEmpty(message = "firstName may not be empty")
     @NotBlank(message = "firstName may not be blank")
-    @Pattern(regexp = "^[A-Za-z]*$", message="firstName may be only characters")
+    @Pattern(regexp = "^[A-Za-z]*$", message = "firstName may be only characters")
     private String firstName;
 
     @NotEmpty(message = "lastName may not be empty")
     @NotBlank(message = "lastName may not be blank")
-    @Pattern(regexp = "^[A-Za-z]*$", message="lastName may be only characters")
+    @Pattern(regexp = "^[A-Za-z]*$", message = "lastName may be only characters")
     private String lastName;
 
     private String organization;
