8366cbe59d1ed075785fda619739c9089b36a71c
gpisa00 <giuseppepisa00@gmail.com>
2020-12-11 19:46:36 +0100

    payment cards api

--- src/main/java/it/arteprogrammazione/restexample/Application.java
+++ src/main/java/it/arteprogrammazione/restexample/Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.hateoas.config.EnableHypermediaSupport;
 
 @SpringBootApplication
-@EnableHypermediaSupport(type= EnableHypermediaSupport.HypermediaType.HAL)
 public class Application {
 
     public static void main(String[] args) {
--- src/main/java/it/arteprogrammazione/restexample/commons/configs/SwaggerConfig.java
+++ src/main/java/it/arteprogrammazione/restexample/commons/configs/SwaggerConfig.java
@@ -2,10 +2,18 @@ package it.arteprogrammazione.restexample.commons.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.hateoas.client.LinkDiscoverer;
+import org.springframework.hateoas.client.LinkDiscoverers;
+import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
+import org.springframework.plugin.core.SimplePluginRegistry;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static springfox.documentation.builders.PathSelectors.regex;
 
@@ -14,6 +22,14 @@ import static springfox.documentation.builders.PathSelectors.regex;
 public class SwaggerConfig {
 
     private static final String BASE_PATH = "it.arteprogrammazione.restexample.controllers.";
+
+    @Primary
+    @Bean
+    public LinkDiscoverers discoverers() {
+        List<LinkDiscoverer> plugins = new ArrayList<>();
+        plugins.add(new CollectionJsonLinkDiscoverer());
+        return new LinkDiscoverers(SimplePluginRegistry.create(plugins));
+    }
 
     @Bean
     public Docket customersApi() {
